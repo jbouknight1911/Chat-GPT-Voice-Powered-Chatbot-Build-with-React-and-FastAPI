a0c05cf72688d75c8f2eb6c6d3c009537be915e2
Jordan Bouknight <89506364+jbouknight1911@users.noreply.github.com>
2023-12-10 18:10:20 -0700

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import Controller from "./components/Controller";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <>
+        <Controller />
+      </>
+    </div>
+    
+  );
+}
+
+export default App;
